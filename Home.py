545a0eac54f5d95298a786d7e81e3c4104a58bf4
felieira <felieira@hotmail.com>
2025-04-10 10:19:54 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -179,10 +179,13 @@ def get_comparison_metrics(data_vendas, data_leads, selected_start, selected_end
                              (data_vendas['Status'] == 'Pago')
         
         # Primeira Sessão
-        primeira_sessao_current = len(data_vendas.loc[current_vendas_mask & 
-                                                    (data_vendas['Recebedores'] == 'Recebedor padrão')])
-        primeira_sessao_previous = len(data_vendas.loc[previous_vendas_mask & 
-                                                     (data_vendas['Recebedores'] == 'Recebedor padrão')])
+        filtro_primeira_sessao = (
+            (data_vendas['Status'] == 'Pago') & 
+            (data_vendas['Valor Capturado (R$)'] < 400) &
+            (data_vendas['Pacote'] == '1ª Compra'))
+        
+        primeira_sessao_current = len(data_vendas.loc[current_vendas_mask & filtro_primeira_sessao])
+        primeira_sessao_previous = len(data_vendas.loc[previous_vendas_mask & filtro_primeira_sessao])
         
         # Primeiro Pacote
         primeiro_pacote_current = len(data_vendas.loc[current_vendas_mask & 
